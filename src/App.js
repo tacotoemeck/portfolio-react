79bbb792999ca24c5ad6781ed8dee466ac00231c
tacotoemeck <wagner.tomek@gmail.com>
2020-04-27 16:20:14 +0100

    Add paralex effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="parallax-wrapper">
+        <div className="App">
+          <header className="App-header">
+            <img
+              className="App-header_img"
+              src={require("./img/trees.jpeg")}
+              alt="background forest"
+            />
+          </header>
+          <main className="mainBody">{/* components including  */}</main>
+          <footer className="footer"></footer>
+        </div>
+      </div>
+      <div className="regular-wrapper"></div>
     </div>
   );
 }
